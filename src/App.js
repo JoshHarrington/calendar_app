3a1121e95e052bac87f25ee1cf57ee57c44de030
Josh Harrington <josh@joshharrington.co.uk>
2018-06-02 15:39:34 +0100

    add select field with some options to pick month and year

--- src/App.js
+++ src/App.js
@@ -5,21 +5,48 @@ import moment from 'moment';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    const todayMonth = moment().month();
+    const todayYear = moment().year();
+    super(props);
+    this.state = {
+      month: todayMonth,
+      year: todayYear
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    const month = "Dec";
-    const year = 2018;
+    const month = this.state.month;
+    const year = this.state.year;
     const currentDate = moment().year(year).month(month);
     const numDaysInCurrentMonth = currentDate.daysInMonth();
     const startDayOfMonth = currentDate.date(1).day();
     const numDaysInMonthMinusFirstWeek = numDaysInCurrentMonth - (7 - startDayOfMonth);
     const numWeeksMinusFirstWeekIfNotWhole = Math.floor(numDaysInMonthMinusFirstWeek / 7);
     const numRemainderDaysEndOfMonth = (numDaysInMonthMinusFirstWeek) % (numWeeksMinusFirstWeekIfNotWhole * 7);
-    const numDaysBeforeAfterMonthExtra = startDayOfMonth + (7 - numRemainderDaysEndOfMonth);
+    let numDaysBeforeAfterMonthExtra = 0;
+    if (numRemainderDaysEndOfMonth == 0) {
+      numDaysBeforeAfterMonthExtra = startDayOfMonth;
+    } else {
+      numDaysBeforeAfterMonthExtra = startDayOfMonth + (7 - numRemainderDaysEndOfMonth);
+    }
     const numDaysMonthTotalPlusBefore = numDaysInCurrentMonth + startDayOfMonth;
     const numDaysMonthTotalPlusExtra = numDaysInCurrentMonth + numDaysBeforeAfterMonthExtra;
     const lastDateOfPreviousMonth = currentDate.date(0).date();
     const firstSundayOfMonth = lastDateOfPreviousMonth - startDayOfMonth + 1;
-
+    console.log(numRemainderDaysEndOfMonth);
     const dayOfWeekSpecial = function (d) {
       if (d === 0) {
         return "Sunday"
@@ -37,6 +64,9 @@ class App extends Component {
         return "Saturday";
       }
     }
+
+    console.log(this.state.month);
+    console.log(this.state.year);
 
     let cells = [];
     let d = 0;
@@ -71,6 +101,22 @@ class App extends Component {
 
     return (
       <div className="App">
+      <form>
+        <select name="month" id="month-num" onChange={this.handleInputChange}>
+          <option value="0">January</option>
+          <option value="1">Feburary</option>
+          <option value="2">March</option>
+          <option value="3">April</option>
+        </select>
+        <select name="year" id="year-num" onChange={this.handleInputChange}>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+        </select>
+      </form>
         <div className="calendar-display">{cells}</div>
       </div>
     );
